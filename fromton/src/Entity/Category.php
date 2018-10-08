ba93bd6f6c62491f0ff4f36339b0410b4a6f8537
baldrani <mael.mayon@free.fr>
2018-10-08 18:49:09 +0200

    Add Catefory and Cheese Repo + Model

---
+++ fromton/src/Entity/Category.php
@@ -0,0 +1,72 @@
+<?php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @UniqueEntity("name")
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ * @ORM\Table(name="`category`")
+ */
+class Category
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORMColumn(type="string", unique=true)
+     */
+    private $name;
+
+    /**
+     * @ORMColumn(type="text")
+     */
+    private $description;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+
+}
