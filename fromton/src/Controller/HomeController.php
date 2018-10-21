b99db4f921202fef8a6083070be89e5ac7821f79
baldrani <mael.mayon@free.fr>
2018-10-22 01:06:55 +0200

    :bug: on cycle twig but we're almost there

--- fromton/src/Controller/HomeController.php
+++ fromton/src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Cheese;
 
 class HomeController extends AbstractController
 {
@@ -13,6 +14,12 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->render('home/index.html.twig');
+        $cheeses = $this->getDoctrine()
+            ->getRepository(Cheese::class)
+            ->findBy([],[], 4);
+
+        dump($cheeses);
+
+        return $this->render('home/index.html.twig', ['cheeses' => $cheeses]);
     }
 }
