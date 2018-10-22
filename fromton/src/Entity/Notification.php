999e8a72db60daef5c7bf4410e201c7153b33650
baldrani <mael.mayon@free.fr>
2018-10-23 00:20:08 +0200

    Change for merge

--- fromton/src/Entity/Notification.php
+++ fromton/src/Entity/Notification.php
@@ -2,10 +2,8 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
- * @UniqueEntity("name")
  * @ORM\Entity(repositoryClass="App\Repository\NotificationRepository")
  * @ORM\Table(name="`notification`")
  */
