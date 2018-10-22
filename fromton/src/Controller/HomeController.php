9d544615f0712e0617826aa3db69f3ea3d1f29bf
baldrani <mael.mayon@free.fr>
2018-10-22 14:54:05 +0200

    Push for poule

--- fromton/src/Controller/HomeController.php
+++ fromton/src/Controller/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends AbstractController
 {
 
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="home", methods={"GET"})
      */
     public function index()
     {
