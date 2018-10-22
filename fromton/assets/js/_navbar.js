c372d091c0b4e65f6019e7a30b1afbccb3443315
baldrani <mael.mayon@free.fr>
2018-10-23 00:48:13 +0200

    Best easter egg ever

--- fromton/assets/js/_navbar.js
+++ fromton/assets/js/_navbar.js
@@ -9,3 +9,11 @@ $('.burgerMenu').on('click', '.fa-bars, .menuClose', function (e) {
     menuSidebar.css('left', '-300px')
   }
 })
+
+$('.notification__number--overninethousand').click(() => {
+    alert('OMAE WA MOU SHINDERU');
+    console.log('%c NANIIII', 'color: red; font-size: 50px')
+    console.log('%c https://www.youtube.com/watch?v=SiMHTK15Pik', 'color: transparent')
+    const naniiii = new Audio("https://www.myinstants.com/media/sounds/nani_Pmxf5n3.mp3")
+    naniiii.play();
+})
