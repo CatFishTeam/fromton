4cd83b4986a86a00e9ca8d3102ca3f5961d225ab
guillaumepn <guillaumepn@free.fr>
2018-12-03 20:19:03 +0100

    hide and show searchbar for mobile

--- fromton/assets/js/components/navbar.js
+++ fromton/assets/js/components/navbar.js
@@ -2,20 +2,30 @@ let burgerMenuState = false;
 const menuSidebar = $('.burgerMenu__links');
 const main = $('main');
 const linkToBurgerMenu = $('.burgerMenu');
+const searchbarContainer = $('.searchbar-container');
+const searchbar = $('#searchbar');
 
 $('.burgerMenu').on('click', '.fa-bars, .menuClose', () => {
     burgerMenuState = !burgerMenuState;
+    // Menu s'ouvre
     if (burgerMenuState) {
         menuSidebar.css('left', '0');
         menuSidebar.removeClass('closed');
-        if (window.innerWidth > 375) {
+        if (window.innerWidth <= 375) {
+            searchbarContainer.animate({width: '100%'}, 250);
+            searchbar.css('padding', '0 10px');
+        } else {
             main.css('margin-left', '300px');
             linkToBurgerMenu.css('left', '-300px');
         }
-    } else {
+    }
+    // Menu se ferme
+    else {
         menuSidebar.css('left', '-300px');
         menuSidebar.addClass('closed');
-        if (window.innerWidth > 375) {
+        if (window.innerWidth <= 375) {
+            searchbarContainer.animate({width: 0}, 100, () => searchbar.css('padding', '0'));
+        } else {
             main.css('margin-left', '0');
             linkToBurgerMenu.css('left', '0');
         }
