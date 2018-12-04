d3e171592c734e97ef0f28c32737015920b37fff
baldrani <mael.mayon@free.fr>
2018-12-04 14:27:35 +0100

    Refactor Quick things

--- fromton/assets/js/components/rating.js
+++ fromton/assets/js/components/rating.js
@@ -26,14 +26,14 @@ stars.rateYo({rating: stars.data('rating')})
         method: "POST",
         body: JSON.stringify(data),
     })
-        .then(function(response) {
-            console.log(response);
-            return response.json();
-        })
-        .then(function(myJson) {
-            window.toastr.success('Votre note a bien été enregistrée !');
-            console.log(JSON.stringify(myJson));
-        });
+    .then(function(response) {
+        console.log(response);
+        return response.json();
+    })
+    .then(function(myJson) {
+        window.toastr.success('Votre note a bien été enregistrée !');
+        console.log(JSON.stringify(myJson));
+    });
 });
 
 $('.show__cheese__rating--global').rateYo({
