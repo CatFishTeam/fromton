b92aaa8fe7a94f9b15d5193d37c766bcb23c18b8
Baldrani <mael.mayon@free.fr>
2018-12-04 14:54:48 +0100

    Fix Home, wasnt the good cheese rendered

--- fromton/src/Controller/HomeController.php
+++ fromton/src/Controller/HomeController.php
@@ -3,25 +3,31 @@
 namespace App\Controller;
 
 use App\Entity\Cheese;
+use App\Repository\CheeseOfTheWeekRepository;
+use App\Repository\CheeseRepository;
+use http\Env\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Twig\Template;
 
 class HomeController extends AbstractController
 {
 
     /**
      * @Route("/", name="home", methods={"GET"}, host="dev.fromton.io")
+     * @param CheeseRepository $cheeseRepository
+     * @param CheeseOfTheWeekRepository $cheeseOfTheWeekRepository
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(CheeseRepository $cheeseRepository, CheeseOfTheWeekRepository $cheeseOfTheWeekRepository)
     {
-        $cheeseRepository = $this->getDoctrine()->getRepository(Cheese::class);
-
         $cheeses = $cheeseRepository->findBy([],[], 4);
         foreach ($cheeses as $cheese){
             $cheese->rating = $cheeseRepository->globalRating($cheese);
         }
 
-        $cheeseOfTheWeek = $cheeseRepository->findOneBy([]);
+        $cheeseOfTheWeek = $cheeseOfTheWeekRepository->findOneBy([]);
+        $cheeseOfTheWeek = $cheeseRepository->find($cheeseOfTheWeek->getCheese());
 
         return $this->render('home/index.html.twig', ['cheeses' => $cheeses, 'cheeseOfTheWeek' => $cheeseOfTheWeek]);
     }
